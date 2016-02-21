33b75399501552e01d1709646a70ea75719f392d
ios-rocks <sbs.friendsorg@gmail.com>
2016-02-21 15:09:21 +0530

    Basic Table View programetically

--- TableViewProgrametically/ViewController.m
+++ TableViewProgrametically/ViewController.m
@@ -8,15 +8,64 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
+//here tableview dtasource : is is for providing data like no of rows
+//delegate : it is for dispay cells (we can customize)
+// this represents the display and behaviour of the cells.
+
+
+@property(strong,nonatomic)UITableView *tableView;
 
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    
+    self.tableView=[[UITableView alloc]initWithFrame:CGRectMake(0, 50, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStyleGrouped];
+    self.tableView.delegate=self;
+    self.tableView.dataSource=self;
+    self.tableView.autoresizingMask=UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+    [self.view addSubview:self.tableView];
+    
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+   
+    return 2;
+
+
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+
+    return 2;
+
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+
+    static NSString *cellIdentifier=@"cell";//user defined
+    
+    UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    if (cell==nil)
+    {
+        
+        cell=[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
+        
+    }
+    cell.selectionStyle=UITableViewStyleGrouped;
+    
+    cell.textLabel.text=@"surendra";
+    cell.detailTextLabel.text=@"name";
+    return cell;
+
 }
 
 - (void)didReceiveMemoryWarning {
